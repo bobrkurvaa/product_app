1842d804be163629f3bd3e595922a5abeb347432
vdenejhnev <97020290+vdenejhnev@users.noreply.github.com>
2024-05-20 21:27:16 +0000

    update order pade v1.1

--- product_app/components/Order.js
+++ product_app/components/Order.js
@@ -1,17 +1,28 @@
+import React, {useState} from 'react';
 import { StyleSheet, View, Text} from 'react-native';
-import Header from './Header';
-import Footer from './Footer';
 
 export default function Order({ navigation, route }) {
     const [products, setProducts] = useState([]);
-    
+
+    let products_array = [];
     route.params.products.forEach(product => {
         fetch(
             'https://sbermarket.ru/api/v2/products?q=' + encodeURI(product.title) + '&sid=' + route.params.store 
         ).then(async(response) => {
             return await response.json();
         }).then((response) => {
-            console.log(response);
+            let product_variations = []
+
+            response.products.forEach(item => {
+                product_variations.push({
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    human_volume: item.human_volume,
+                    volume: item.volume,
+                    volume_type: item.type
+                })
+            })
         })
     });
     
