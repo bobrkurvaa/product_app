ddf92396641efb13b5715c7f221abe42d7241de6
vdenejhnev <97020290+vdenejhnev@users.noreply.github.com>
2024-05-21 11:37:18 +0000

    update order page v2.4

--- product_app/components/Whishlist.js
+++ product_app/components/Whishlist.js
@@ -3,6 +3,24 @@ import Header from './Header';
 import Footer from './Footer';
 
 export default function Whishlist() {
+    fetch(
+      'https://sbermarket.ru/api/v2/phone_confirmations',
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: {
+          phone: 'sz4t/sr5QO3gT6vDJm4yPbPMeD0ZkurgHS4/gSpYtxQ='
+        }
+      }
+    ).then(response => {
+      return response.text();
+    }).then(response => {
+      console.log(response)
+    });
+
     return (
       <View>
       </View>
