e5f9af640c34ae2595a47caa6f449e2424631f69
vdenejhnev <97020290+vdenejhnev@users.noreply.github.com>
2024-05-21 05:53:59 +0000

    update order page v1.8

--- product_app/components/Order.js
+++ product_app/components/Order.js
@@ -1,23 +1,23 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text, Image} from 'react-native';
+import { StyleSheet, View, ScrollView, Text, Image} from 'react-native';
 
 export default function Order({ navigation, route }) {
     const [products, setProducts] = useState([]);
     const [products_array, setProductsArray] = useState([]);
 
-    route.params.products.forEach((product, key) => {
-        fetch(
+    route.params.products.forEach(async (product, key) => {
+        await fetch(
             'https://sbermarket.ru/api/v2/products?q=' + encodeURI(product.title) + '&sid=' + route.params.store 
         ).then(async(response) => {
             return await response.json();
-        }).then((response) => {
+        }).then(async(response) => {
             let variations = [];
 
             response.products.forEach(item => {
                 variations.push({
                     id: item.id,
                     name: item.name,
-                    image: item.images[0].small_url,
+                    image: item.images[0].product_url,
                     price: item.price,
                     human_volume: item.human_volume,
                     volume: item.volume,
@@ -25,9 +25,9 @@ export default function Order({ navigation, route }) {
                 })
             })
 
-            return variations;
-        }).then((variations) => {
-            setProductsArray((list) => {
+            return await variations;
+        }).then(async (variations) => {
+            await setProductsArray((list) => {
                 return [
                     {
                         search_product: product.title,
@@ -38,7 +38,7 @@ export default function Order({ navigation, route }) {
             })
 
             if (products_array.length == route.params.products.length) {
-                setProducts(products_array);
+                await setProducts(products_array);
             }
         });
     });
@@ -50,17 +50,22 @@ export default function Order({ navigation, route }) {
         {products.map((item) =>
             <View style={styles.recipe_product}>
                 <Text style={styles.recipe_product_title}>{item.search_product}</Text>
-                {item.product_variations.map((product_variation) =>
-                    <View style={styles.order_product_variation}>
-                        <Image 
-                            style={styles.order_product_variation_image}
-                            source={{
-                                uri: product_variation.image
-                            }}
-                        />
-                        <Text style={styles.stores_preloader_text}>{product_variation.name}</Text>
-                    </View>
-                )}
+                <ScrollView
+                    horizontal={true} 
+                    style={styles.order_product_variations}
+                >
+                    {item.product_variations.map((product_variation) =>
+                        <View style={styles.order_product_variation}>
+                            <Image 
+                                style={styles.order_product_variation_image}
+                                source={{
+                                    uri: product_variation.image
+                                }}
+                            />
+                            <Text style={styles.stores_preloader_text}>{product_variation.name}</Text>
+                        </View>
+                    )}
+                </ScrollView>
             </View>
         )}
       </View>
@@ -68,6 +73,8 @@ export default function Order({ navigation, route }) {
 }
 
 const styles = StyleSheet.create({
+    order_product_variations: {
+    },
     order_product_variation_image: {
         width: 64,
         height: 64
