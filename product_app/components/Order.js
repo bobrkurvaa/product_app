707d6f26558b07c8bfc6ad4bd9d1a88c699a1ebe
vdenejhnev <97020290+vdenejhnev@users.noreply.github.com>
2024-05-21 05:20:48 +0000

    update order page v1.5

--- product_app/components/Order.js
+++ product_app/components/Order.js
@@ -1,39 +1,63 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Image} from 'react-native';
 
 export default function Order({ navigation, route }) {
     const [products, setProducts] = useState([]);
 
-    let products_array = [];
-    route.params.products.forEach(product => {
+    var products_array = [];
+
+    route.params.products.forEach((product, key) => {
         fetch(
             'https://sbermarket.ru/api/v2/products?q=' + encodeURI(product.title) + '&sid=' + route.params.store 
         ).then(async(response) => {
             return await response.json();
         }).then((response) => {
-            let product_variations = []
+            let variations = [];
 
             response.products.forEach(item => {
-                product_variations.push({
+                variations.push({
                     id: item.id,
                     name: item.name,
+                    image: item.images.small_url,
                     price: item.price,
                     human_volume: item.human_volume,
                     volume: item.volume,
                     volume_type: item.type
                 })
             })
+
+            return variations;
+        }).then(variations => {
+            products_array.push({
+                search_product: product.title,
+                product_variations: variations
+            })
+        }).then(()=>{
+            if (products_array.length == route.params.products.length) {
+                console.log(products_array)
+                setProducts(products_array);
+            }
         })
     });
-    
 
     return (
       <View>
         <Text>Создание заказа</Text>
         <Text>{route.params.store}</Text>
-        {route.params.products.map((item) =>
+        {products.map((item) =>
             <View style={styles.recipe_product}>
-                <Text style={styles.recipe_product_title}>{item.title}</Text>
+                <Text style={styles.recipe_product_title}>{item.search_product}</Text>
+                {item.product_variations.map((product_variation) =>
+                    <View style={styles.order_product_variation}>
+                        <Image 
+                            style={styles.order_product_variation_image}
+                            source={{
+                                uri: product_variation.image
+                            }}
+                        />
+                        <Text style={styles.stores_preloader_text}>{product_variation.name}</Text>
+                    </View>
+                )}
             </View>
         )}
       </View>
@@ -41,5 +65,8 @@ export default function Order({ navigation, route }) {
 }
 
 const styles = StyleSheet.create({
-
+    order_product_variation_image: {
+        width: 64,
+        height: 64
+    }
 });
